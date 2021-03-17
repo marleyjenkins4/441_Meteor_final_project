280e1a13c5bf4243ce40507efc31f2a959f4cf33
marleyjenkins4 <jenkinsm19@winthrop.edu>
2021-03-17 15:58:22 -0400

    react and js intro video notes

--- reddit/client/main.js
+++ reddit/client/main.js
@@ -8,15 +8,27 @@ import {Meteor} from 'meteor/meteor'; //named export from Meteor
   //2. npm libraries providig the name 'react-dom'
   //3. metoer modules 'metoer/meteor' . the meteor / removes name conflicts
 
-Meteor.startup(function(){
-  let name= 'newman';
-  let jsx = (
-          <div>
-              <p> this is from client/main.js </p>
-              <p> Hello {name} </p>
-           </div>
-         );
 
+//meteor.startup wait for the dom to be renders before doing anything
+Meteor.startup(function(){ //this takes a function as its one and onyl argument
+  //we now need to render jsx which is javascript
+  //let jsx=       <p> this is from client/main.js </p>
+
+  let name= 'newman';
+  let jsx =  <p> Hello {name} </p>
+
+  //the following causes problems b/c it violates the rule that you argument
+  //you are only allowed to have one root element
+  //let jsx= <p> this is form client/main.js </p> <p> hello {name}</p>;
+  //instead we need to wrap it in a single root element i.e <div>
+
+  jsx= <div> <p> this is from client/main.js</p><p> hello {name}!</p></div>;
+  jsx= (
+    <div>
+      <p> this is from client/main.js</p>
+      <p> hello {name}!</p>
+      </div>
+  );
   ReactDOM.render (jsx,document.getElementById('content'));
 });
 
