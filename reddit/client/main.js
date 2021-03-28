4f1a9ff3e330e1abc2fa74779f583fa24ab9eb15
marleyjenkins4 <jenkinsm19@winthrop.edu>
2021-03-28 17:54:37 -0400

    dynamic db entry challenge

--- reddit/client/main.js
+++ reddit/client/main.js
@@ -25,11 +25,22 @@ const renderPosts= function(passed_posts){
   return formattedPosts;
 };
  const processFormDataFunction= function(event){
-   event.preventDefault();
+   // the event (sometimers e ) parameter is a default event handler object
+   //that is passed by in the browser  when an event occurs
+   //this is an important argument because it allows us to access the topic name
+   //which we need in order to insert a new topic into the db
+   event.preventDefault(); //stops the page from reloading
    let newTopic=event.target.formInputNameAttribute.value;
+   //event.target grabs the target element - the form in this case which allows us
+   // to grab any of its inputs referencing by name (.formINputNameAttribute)
+   //value gets us the value
   console.log(newTopic);
   if(newTopic){
-    event.target.formInputNameAttribute.value='';
+    event.target.formInputNameAttribute.value=''; //clears input box
+    UP_Collection_Access.insert({
+      topic: newTopic,
+      votes: 0,
+    });
   }
 
  };
